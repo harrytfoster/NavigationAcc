8d2ca53adf91662b7dcc7ee8cb9ab1acb700cca2
Harry <harrythomasfoster@gmail.com>
2024-03-22 20:14:12 +0000

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,5 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation ("org.osmdroid:osmdroid-android:6.1.17")
 }
--- app/src/main/java/com/example/navigationacc/MainActivity.kt
+++ app/src/main/java/com/example/navigationacc/MainActivity.kt
@@ -16,22 +16,61 @@ import com.example.navigationacc.ui.theme.NavigationAccTheme
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.TextField
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun ComposableA(addPersonCallback: ()->Unit) { // Callback needs to be entered here
-    Column{
-    Text("First Composable")
-    Button(onClick = { addPersonCallback() } ) { // Button calls callback
-        Text("Next Compose")
-    }
+fun ViewMap(SettingsCallback: ()->Unit, geoPoint: GeoPoint) { // Callback needs to be entered here
+    BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+        val context =
+            LocalContext.current
+        AndroidView(
+            modifier = Modifier.fillMaxSize(),
+            factory = { context ->
+                MapView(context).apply()
+                {
+                    controller.setCenter(geoPoint) // Set geopoints
+                    controller.setZoom(14.0) // Set zoom
+                    isClickable = true
+                } // Makes map clickable
+            }
+
+        )
+        Button(onClick = { SettingsCallback() },
+            modifier = Modifier // Updates search lat and long on click
+                .align(Alignment.BottomStart)
+                .padding(16.dp)) { ("To Settings") }
     }
 }
 
+
 @Composable
-fun ComposableB(MainScreenCallback: ()->Unit){
+fun ViewSettings(MapScreenCallback: ()->Unit, geoPoint: GeoPoint){
     Column{
     Text("Other Composable")
-    Button(onClick = { MainScreenCallback() }) {
+    Button(onClick = { MapScreenCallback() }) {
         Text("Go Back")
     }
     }
@@ -41,17 +80,19 @@ fun ComposableB(MainScreenCallback: ()->Unit){
 fun ParentComposable() {
     val navController = rememberNavController() // Sets navigation controller
 
-    NavHost(navController = navController, startDestination = "mainScreen") {// Select Start Screen
-        composable("mainScreen") {// Make a screen and name it
-            ComposableA(addPersonCallback = { // This callback would navigate to screen named "addPersonScreen"
-                navController.navigate("addPersonScreen")
-            })
+    var geoPoint by remember { mutableStateOf(GeoPoint(0.0,0.0)) }
+
+    NavHost(navController = navController, startDestination = "MapScreen") {// Select Start Screen
+        composable("MapScreen") {// Make a screen and name it
+            ViewMap(SettingsCallback = { // This callback would navigate to screen named "addPersonScreen"
+                navController.navigate("SettingScreen")
+            }, geoPoint)
         }
 
-        composable("addPersonScreen") {
-            ComposableB(MainScreenCallback = { // This callback will pop the screen on top of the stack
+        composable("SettingScreen") {
+            ViewSettings(MapScreenCallback = { // This callback will pop the screen on top of the stack
                 navController.popBackStack()
-            })
+            }, geoPoint)
         }
     }
 }
@@ -59,6 +100,7 @@ fun ParentComposable() {
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Configuration.getInstance().load(this, getPreferences(MODE_PRIVATE))
         setContent {
             ParentComposable()
         }
